2240f2be74ec50cce9d3d988eafc6d5777645cf1
Jędrzej Witaszak <jedrzej.witaszak@kinguin.net>
2018-06-25 15:19:02 +0200

    spring initializr

---
+++ src/main/java/com/recruitment/consdata/ConsdataApplication.java
@@ -0,0 +1,12 @@
+package com.recruitment.consdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsdataApplication.class, args);
+	}
+}
